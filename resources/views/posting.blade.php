60e2d10b3be06e93800a0476c62891a381fddd01
Betti <bett0810@gmail.com>
2021-12-07 21:17:13 +0100

    blades updated

--- resources/views/posting.blade.php
+++ resources/views/posting.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
+
 <html lang="hu">
 
 
 
 <head>
     <title>My first laravel blog</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
